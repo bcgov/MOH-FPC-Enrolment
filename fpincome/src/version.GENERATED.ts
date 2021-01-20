8ba2fc2c2f8833c3599f1a5580f6c9d605978e67
kristin reed <kristin.reed@maximusbc.ca>
2021-01-20 00:00:06 -0500

    chore: modify url path

---
+++ fpincome/src/version.GENERATED.ts
@@ -0,0 +1,11 @@
+// DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
+export const gitCommit = '076011f';
+export const buildTime = '1/19/2021 at 7:41:12 PM';
+/** App version retrieved from package.json. */
+export const projectVersion = '2.0.3';
+/** Human readable message  */
+export const message =
+  'Canonical Version: 076011f - 1/19/2021 at 7:41:12 PM (v2.0.3)' +
+  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/076011f';
+/** If true, other values should be present. */
+export const success = true;
