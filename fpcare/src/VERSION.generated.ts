c5e5bbafa3ff704e384d12e27668252c0b53a199
kristin reed <kristin.reed@maximusbc.ca>
2021-01-29 18:39:33 -0500

    fix: remove trimmed.length

--- fpcare/src/VERSION.generated.ts
+++ fpcare/src/VERSION.generated.ts
@@ -1,9 +1,9 @@
 //DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '5fb1bf5';
-export const buildTime = '1/29/2021 at 1:59:10 PM';
+export const gitCommit = '0c4c81c';
+export const buildTime = '1/29/2021 at 3:36:25 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '4.0.1';
 /** Human readable message  */
-export const message = 'Canonical Version: 5fb1bf5 - 1/29/2021 at 1:59:10 PM (v4.0.1) \nhttp://github.com/bcgov/moh-fpcare/commits/5fb1bf5';
+export const message = 'Canonical Version: 0c4c81c - 1/29/2021 at 3:36:25 PM (v4.0.1) \nhttp://github.com/bcgov/moh-fpcare/commits/0c4c81c';
 /** If true, other values should be present. */
 export const success = true;
--- fpincome/src/version.GENERATED.ts
+++ fpincome/src/version.GENERATED.ts
@@ -1,11 +1,11 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'fbb134a';
-export const buildTime = '1/29/2021 at 1:35:40 PM';
+export const gitCommit = '0c4c81c';
+export const buildTime = '1/29/2021 at 3:36:38 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.0.4';
 /** Human readable message  */
 export const message =
-  'Canonical Version: fbb134a - 1/29/2021 at 1:35:40 PM (v2.0.4)' +
-  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/fbb134a';
+  'Canonical Version: 0c4c81c - 1/29/2021 at 3:36:38 PM (v2.0.4)' +
+  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/0c4c81c';
 /** If true, other values should be present. */
 export const success = true;
