5fb1bf5f61526cfa9a762d923eb499ac9831f1f0
kristin reed <kristin.reed@maximusbc.ca>
2021-01-29 16:38:03 -0500

    chore: commit version file and changelog

--- fpincome/src/version.GENERATED.ts
+++ fpincome/src/version.GENERATED.ts
@@ -1,11 +1,11 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '076011f';
-export const buildTime = '1/19/2021 at 7:41:12 PM';
+export const gitCommit = 'fbb134a';
+export const buildTime = '1/29/2021 at 1:35:40 PM';
 /** App version retrieved from package.json. */
-export const projectVersion = '2.0.3';
+export const projectVersion = '2.0.4';
 /** Human readable message  */
 export const message =
-  'Canonical Version: 076011f - 1/19/2021 at 7:41:12 PM (v2.0.3)' +
-  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/076011f';
+  'Canonical Version: fbb134a - 1/29/2021 at 1:35:40 PM (v2.0.4)' +
+  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/fbb134a';
 /** If true, other values should be present. */
 export const success = true;
