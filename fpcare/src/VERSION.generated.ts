26a5bea01290cc672a1aa1e988d3167099b3f1c2
kristin reed <kristin.reed@maximusbc.ca>
2021-03-02 20:30:08 -0500

    chore: commit version file

--- fpcare/src/VERSION.generated.ts
+++ fpcare/src/VERSION.generated.ts
@@ -1,10 +1,10 @@
 //DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'ddfec1d';
-export const buildTime = '2/26/2021 at 11:56:29 AM';
+export const gitCommit = 'a42d7f7';
+export const buildTime = '3/2/2021 at 5:30:07 PM';
 /** App version retrieved from package.json. */
-export const projectVersion = '4.1.1';
+export const projectVersion = '4.1.2';
 /** Human readable message  */
-export const message = 'Canonical Version: ddfec1d - 2/26/2021 at 11:56:29 AM (v4.1.1) ' +
-'\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/ddfec1d';
+export const message = 'Canonical Version: a42d7f7 - 3/2/2021 at 5:30:07 PM (v4.1.2) ' +
+'\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/a42d7f7';
 /** If true, other values should be present. */
 export const success = true;
