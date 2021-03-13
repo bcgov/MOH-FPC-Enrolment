6bacd13210fad86a5ff0673298ece42dae788023
kristin reed <kristin.reed@maximusbc.ca>
2021-03-12 20:50:52 -0500

    fix(access): landmarks

--- fpincome/src/version.GENERATED.ts
+++ fpincome/src/version.GENERATED.ts
@@ -1,11 +1,11 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '6aeb52b';
-export const buildTime = '2/14/2021 at 5:13:52 PM';
+export const gitCommit = 'a29633a';
+export const buildTime = '3/12/2021 at 5:36:22 PM';
 /** App version retrieved from package.json. */
-export const projectVersion = '2.0.6';
+export const projectVersion = '2.1.1';
 /** Human readable message  */
 export const message =
-  'Canonical Version: 6aeb52b - 2/14/2021 at 5:13:52 PM (v2.0.6)' +
-  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/6aeb52b';
+  'Canonical Version: a29633a - 3/12/2021 at 5:36:22 PM (v2.1.1)' +
+  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/a29633a';
 /** If true, other values should be present. */
 export const success = true;
