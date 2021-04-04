ffc512e2b71665aa492b3f75b104fa8ff3d4120f
kristin reed <kristin.reed@maximusbc.ca>
2021-04-04 15:24:00 -0400

    chore(dep): update common lib

--- fpincome/src/version.GENERATED.ts
+++ fpincome/src/version.GENERATED.ts
@@ -1,11 +1,11 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'a29633a';
-export const buildTime = '3/12/2021 at 5:36:22 PM';
+export const gitCommit = '7e7bb91';
+export const buildTime = '4/4/2021 at 12:19:15 PM';
 /** App version retrieved from package.json. */
 export const projectVersion = '2.1.1';
 /** Human readable message  */
 export const message =
-  'Canonical Version: a29633a - 3/12/2021 at 5:36:22 PM (v2.1.1)' +
-  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/a29633a';
+  'Canonical Version: 7e7bb91 - 4/4/2021 at 12:19:15 PM (v2.1.1)' +
+  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/7e7bb91';
 /** If true, other values should be present. */
 export const success = true;
