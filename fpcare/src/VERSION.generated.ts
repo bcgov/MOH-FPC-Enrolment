49e38ba444408f5f406a2f4bd507c0a056cf1798
Cyril Bravo <cyril.bravo@maximuscanada.ca>
2021-07-08 10:54:29 -0700

    chore: commit version file

--- fpcare/src/VERSION.generated.ts
+++ fpcare/src/VERSION.generated.ts
@@ -1,10 +1,10 @@
 //DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'fc8710f';
-export const buildTime = '4/9/2021 at 1:09:43 PM';
+export const gitCommit = 'fb51dcc';
+export const buildTime = '7/8/2021 at 10:54:28 AM';
 /** App version retrieved from package.json. */
-export const projectVersion = '4.1.3';
+export const projectVersion = '4.1.4';
 /** Human readable message  */
-export const message = 'Canonical Version: fc8710f - 4/9/2021 at 1:09:43 PM (v4.1.3) ' +
-'\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/fc8710f';
+export const message = 'Canonical Version: fb51dcc - 7/8/2021 at 10:54:28 AM (v4.1.4) ' +
+'\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/fb51dcc';
 /** If true, other values should be present. */
 export const success = true;
