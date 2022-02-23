e7afd18947d5cb1a43cd7ad0f71e58fd0cac082d
Cyril Bravo <cyril.bravo@maximuscanada.ca>
2022-02-23 12:50:29 -0800

    chore: commit changelog

--- fpincome/src/version.GENERATED.ts
+++ fpincome/src/version.GENERATED.ts
@@ -1,11 +1,11 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '03de88f';
-export const buildTime = '2/7/2022 at 12:49:47 PM';
+export const gitCommit = '014dc7d';
+export const buildTime = '2/23/2022 at 12:50:26 PM';
 /** App version retrieved from package.json. */
-export const projectVersion = '2.1.16';
+export const projectVersion = '2.1.17';
 /** Human readable message  */
 export const message =
-  'Canonical Version: 03de88f - 2/7/2022 at 12:49:47 PM (v2.1.16)' +
-  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/03de88f';
+  'Canonical Version: 014dc7d - 2/23/2022 at 12:50:26 PM (v2.1.17)' +
+  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/014dc7d';
 /** If true, other values should be present. */
 export const success = true;
