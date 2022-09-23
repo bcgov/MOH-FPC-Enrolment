ad88feb74d4ea72956f44cde68da80c21b4f1607
Bryce Mcmath <bryce.mcmath@maximuscanada.ca>
2022-09-23 15:53:30 -0700

    chore: commit version file

--- fpcare/src/VERSION.generated.ts
+++ fpcare/src/VERSION.generated.ts
@@ -1,10 +1,10 @@
 //DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'bae2b65';
-export const buildTime = '9/1/2022 at 2:12:52 PM';
+export const gitCommit = '6f9bd8d';
+export const buildTime = '9/23/2022 at 3:53:29 PM';
 /** App version retrieved from package.json. */
-export const projectVersion = '4.2.0';
+export const projectVersion = '4.2.1';
 /** Human readable message  */
-export const message = 'Canonical Version: bae2b65 - 9/1/2022 at 2:12:52 PM (v4.2.0) ' +
-'\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/bae2b65';
+export const message = 'Canonical Version: 6f9bd8d - 9/23/2022 at 3:53:29 PM (v4.2.1) ' +
+'\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/6f9bd8d';
 /** If true, other values should be present. */
 export const success = true;
