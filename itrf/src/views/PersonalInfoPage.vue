7c59f1923253e83903a08a5a414f041146924df1
cyrilbravo <cyrildbravo@gmail.com>
2024-01-02 16:03:00 -0800

    feat: added fields and tip box for Personal Info Page

--- itrf/src/views/PersonalInfoPage.vue
+++ itrf/src/views/PersonalInfoPage.vue
@@ -4,16 +4,92 @@
         <PageContent>
             <div class="container pt-3 pt-sm-5 mb-5">
                 <h1>Personal Information</h1>
+                <hr/>
+                <p>Enter your name as it appears in your BC Services Card</p>
+                <Input
+                    :label="'First name'"
+                    :className="'mt-3'"
+                    v-model="firstName"
+                    :required="true"
+                />
+                <Input
+                    :label="'Last name'"
+                    :className="'mt-3'"
+                    v-model="lastName"
+                    :required="true"
+                />
+                <div class="row">
+                    <div class="col-sm-7">
+                    <DateInput
+                        label='Birthdate'
+                        className='mt-3'
+                        v-model='birthDate'
+                        :required="true"
+                    />
+                    <Input
+                        :label="'Personal Health Number (PHN)'"
+                        :className="'mt-3'"
+                        v-model="phn"
+                        :required="true"
+                    />
+                    <Input
+                        :label="'Pharmacare Registration Number (optional)'"
+                        :className="'mt-3'"
+                        v-model="regNum"
+                        :required="true"
+                    />
+                    </div>
+                    <div class="col-sm-5">
+                        <TipBox title="Tip: PHN number">
+                            <p>The 10 digit number that can be found on the back of your <a href="https://www2.gov.bc.ca/gov/content/health/health-drug-coverage/msp/bc-residents/personal-health-identification/your-bc-services-card" target="_blank">BC Services Card</a> (or front of the CareCard if you still have one).</p>
+                            <div class="bcid-container">
+                                <div class="bcid-image-container">
+                                <img src="../images/bcid-sample-front.png"
+                                    alt="BC Services Card sample front"
+                                    class="bcid"/>
+                                </div>
+                                <div class="bcid-image-container">
+                                <img src="../images/bcid-sample-back.png"
+                                    alt="BC Services Card sample back"
+                                    class="bcid"/>
+                                </div>
+                            </div>
+                        </TipBox>
+                    </div>
+                </div>  
             </div>
         </PageContent>
         <ContinueBar @continue="nextPage()" :buttonLabel="'Continue'" />
     </div>
 </template>
+
+<style scoped>
+.mt-3 {
+    font-weight: bolder;
+    width: 400px;
+}
+.bcid-container {
+  display: flex;
+  flex-flow: row wrap;
+}
+.bcid-image-container {
+  width: 50%;
+  padding: 5px;
+}
+.bcid {
+  width: auto;
+  max-width: 100%;
+  height: auto;
+}
+</style>
   
 <script>
 import ProgressBar from '../components/ProgressBar.vue';
 import PageContent from '../components/PageContent.vue';
 import ContinueBar from '../components/ContinueBar.vue';
+import Input from '../components/Input.vue';
+import DateInput from '../components/DateInput.vue';
+import TipBox from '../components/TipBox.vue';
 import { stepRoutes, routes } from '../router/index';
 import pageStateService from '../services/page-state-service.js';
 
@@ -22,11 +98,19 @@ export default {
     components: {
         ProgressBar,
         PageContent,
-        ContinueBar
+        ContinueBar,
+        Input,
+        DateInput,
+        TipBox
     },
     data: () => {
         return {
             stepRoutes,
+            firstName: "",
+            lastName: "",
+            birthDate: "",
+            phn: "",
+            regNum: "",
         };
     },
     created() {
