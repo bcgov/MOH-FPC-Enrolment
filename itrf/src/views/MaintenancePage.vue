485d9c716a331f2a6f0c14111a98ee0cae688b51
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-02-23 16:01:50 -0800

    feat: add maintenance page

--- itrf/src/views/MaintenancePage.vue
+++ itrf/src/views/MaintenancePage.vue
@@ -1,3 +1,33 @@
 <template>
-<div>placeholder maintenance page</div>
-</template>
+    <div>
+      <PageContent>
+        <div class="container pt-3 pt-sm-5 mb-5">
+          <h1>Maintenance Mode</h1>
+          <hr/>
+          <p>{{ maintenanceMessage }}</p>
+        </div>
+      </PageContent>
+    </div>
+  </template>
+  
+  <script>
+  import PageContent from '../components/PageContent.vue';
+  
+  export default {
+    name: 'MaintenancePage',
+    components: {
+      PageContent,
+    },
+    data: () => {
+      return {
+        maintenanceMessage:
+          "This application is currently unavailable due to maintenance. Please try again later.",
+      };
+    },
+    created() {
+      if (this && this.$store && this.$store.state && this.$store.state.maintenanceMessage) {
+        this.maintenanceMessage = this.$store.state.maintenanceMessage;
+      }
+    },
+  }
+  </script>
