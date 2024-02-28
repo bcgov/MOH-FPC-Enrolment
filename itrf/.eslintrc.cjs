08181d79ca962552094086ac5ac36dd421b382da
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-02-27 16:23:47 -0800

    feat: add rule, parser options to .eslintrc.cjs

--- itrf/.eslintrc.cjs
+++ itrf/.eslintrc.cjs
@@ -4,7 +4,11 @@ module.exports = {
   },
   extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
   rules: {
+    "no-unused-vars": "warn"
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
+  },
+  parserOptions: {
+    ecmaVersion: 13, //allows eslint to correctly parse class properties, eg. page-state-service.js
   },
 };
