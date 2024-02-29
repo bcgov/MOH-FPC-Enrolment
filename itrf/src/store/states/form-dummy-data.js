4f58f6746a8ed316101983eb05c885e24816eb68
Skyler <skyler.mcnamee@hibc.gov.bc.ca>
2024-02-29 12:27:03 -0800

    fix: fix dummy data date

--- itrf/src/store/states/form-dummy-data.js
+++ itrf/src/store/states/form-dummy-data.js
@@ -2,7 +2,7 @@ export default {
     firstName: "KDAF",
     lastName: "WCL",
 	phn: "950 297 6289",
-	birthdate: new Date("1985-11-23"),
+	birthdate: new Date("Sat Nov 23 1985 16:00:00 GMT-0800"),
     applicantHasFiledIncomeTaxReturn: "Y",
     applicantHasSpouse: "N",
 };
