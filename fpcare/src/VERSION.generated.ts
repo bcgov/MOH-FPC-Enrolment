fdbdfdac53b52adc82455f31960adca26fafa2c2
Laurent Beique <laurent.beique@dxcas.com>
2026-09-09 16:03:27 -0700

    chore: version bump to 4.2.4

--- fpcare/src/VERSION.generated.ts
+++ fpcare/src/VERSION.generated.ts
@@ -1,10 +1,10 @@
 //DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '2424faf';
-export const buildTime = '4/4/2024 at 8:39:22 AM';
+export const gitCommit = '3cd301e';
+export const buildTime = '2026-09-09 at 3:26:57 p.m.';
 /** App version retrieved from package.json. */
-export const projectVersion = '4.2.3';
+export const projectVersion = '4.2.4';
 /** Human readable message  */
-export const message = 'Canonical Version: 2424faf - 4/4/2024 at 8:39:22 AM (v4.2.3) ' +
-'\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/2424faf';
+export const message = 'Canonical Version: 3cd301e - 2026-09-09 at 3:26:57 p.m. (v4.2.4) ' +
+'\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/3cd301e';
 /** If true, other values should be present. */
 export const success = true;
