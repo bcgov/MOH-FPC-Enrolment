5160a295311cb2bbc78336ec71196072bd687a2b
Laurent Beique <laurent.beique@dxcas.com>
2026-09-21 09:11:58 -0700

    FPIncome version bump to 2.1.21 (#164)
    * chore: commit changelog
    
    * fix: add backticks to html characters in CHANGELOG

--- fpincome/src/version.GENERATED.ts
+++ fpincome/src/version.GENERATED.ts
@@ -1,11 +1,11 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'fdbdfda';
-export const buildTime = '2026-09-15 at 7:41:08 p.m.';
+export const gitCommit = 'b39dc1a';
+export const buildTime = '2026-09-20 at 7:44:56 p.m.';
 /** App version retrieved from package.json. */
-export const projectVersion = '2.1.20';
+export const projectVersion = '2.1.21';
 /** Human readable message  */
 export const message =
-  'Canonical Version: fdbdfda - 2026-09-15 at 7:41:08 p.m. (v2.1.20)' +
-  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/fdbdfda';
+  'Canonical Version: b39dc1a - 2026-09-20 at 7:44:56 p.m. (v2.1.21)' +
+  '\nhttps://github.com/bcgov/MOH-FPC-Enrolment/commits/b39dc1a';
 /** If true, other values should be present. */
 export const success = true;
